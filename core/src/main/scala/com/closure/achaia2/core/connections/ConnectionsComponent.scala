40e47bee145099b6b9bf799e57982f0a738a2c96
Mikołaj Wielocha <mikolaj.wielocha@opigram.com>
2014-01-31 13:21:52 +0100

    explorer view

--- core/src/main/scala/com/closure/achaia2/core/connections/ConnectionsComponent.scala
+++ core/src/main/scala/com/closure/achaia2/core/connections/ConnectionsComponent.scala
@@ -2,14 +2,26 @@ package com.closure.achaia2.core.connections
 
 import org.openide.windows.TopComponent
 import java.awt.BorderLayout
+import org.openide.explorer.{ExplorerUtils, ExplorerManager}
+import org.openide.nodes.AbstractNode
+import org.openide.nodes.FilterNode.Children
 
 /**
  * Created by mwielocha on 30.01.2014.
  */
-class ConnectionsComponent extends TopComponent {
+class ConnectionsComponent extends TopComponent with ExplorerManager.Provider {
+
+    @transient
+    private val explorerManager = new ExplorerManager
+
+    def getExplorerManager: ExplorerManager = explorerManager
+
+
+    associateLookup(ExplorerUtils.createLookup(explorerManager, getActionMap()));
+//    explorerManager.setRootContext(new AbstractNode(new Children()))
+    explorerManager.getRootContext().setDisplayName("Marilyn Monroe's Movies")
 
     setName("A component")
-    setLayout(new BorderLayout())
 
 
 }
